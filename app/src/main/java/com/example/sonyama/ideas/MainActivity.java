e5622a0b5621ebc3d5a2894488c9d649006ba9e6
ngoduyson <ngo.duy.son@framgia.com>
2016-01-13 17:35:33 +0700

    commit1

--- app/src/main/java/com/example/sonyama/ideas/MainActivity.java
+++ app/src/main/java/com/example/sonyama/ideas/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -42,11 +43,19 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch(id) {
+            case R.id.fetch_action:
+                fetchChannelList();
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Fetch JSON-data from server
+     */
+    public void fetchChannelList() {
+        Log.i("test", "show file");
     }
 }
